819e64330b44848fdbcde622993423c82baf07aa
dakshinya6-Git <dakshinyakondapalli06@gmail.com>
2026-03-04 09:31:32 +0530

    Update Multistage.py

--- Multistage.py
+++ Multistage.py
@@ -1,7 +1,8 @@
 INF = 9999
-
 def multistage(cost, n):
     d = [0]*n
+    p = [0]*n
+
     d[n-1] = 0
 
     for i in range(n-2, -1, -1):
@@ -9,9 +10,18 @@ def multistage(cost, n):
         for j in range(i+1, n):
             if cost[i][j] != INF and cost[i][j] + d[j] < m:
                 m = cost[i][j] + d[j]
+                p[i] = j
         d[i] = m
 
-    return d[0]
+    print("Minimum cost:", d[0])
+
+    print("Path:", end=" ")
+    v = 0
+    print(v+1, end=" ")
+    while v != n-1:
+        v = p[v]
+        print("->", v+1, end=" ")
+    print()
 
 
 n = int(input("Enter number of vertices: "))
@@ -21,7 +31,4 @@ cost = []
 for i in range(n):
     row = list(map(int, input().split()))
     cost.append(row)
-
-ans = multistage(cost, n)
-
-print("Minimum cost from source to destination:", ans)
+multistage(cost, n)
